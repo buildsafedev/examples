855362865494d1769a45bb5e930b683b70f9b89c
House <house@buildsafe.dev>
2024-07-22 00:57:12 +0530

    pin deps
    Signed-off-by: House <house@buildsafe.dev>

--- rust/bsf/flake.nix
+++ rust/bsf/flake.nix
@@ -3,11 +3,11 @@
 	description = "";
 	
 	inputs = {
-		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4.url = "github:nixos/nixpkgs/7445ccd775d8b892fc56448d17345443a05f7fb4";
 		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7.url = "github:nixos/nixpkgs/1ebb7d7bba2953a4223956cfb5f068b0095f84a7";
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14.url = "github:nixos/nixpkgs/ac5c1886fd9fe49748d7ab80accc4c847481df14";
 		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746.url = "github:nixos/nixpkgs/a731d0cb71c58f56895f71a5b02eda2962a46746";
 		 nixpkgs-d919897915f0f91216d2501b617d670deee993a0.url = "github:nixos/nixpkgs/d919897915f0f91216d2501b617d670deee993a0";
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4.url = "github:nixos/nixpkgs/7445ccd775d8b892fc56448d17345443a05f7fb4";
 			
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 		
@@ -28,11 +28,11 @@
 	
 	
 	 nix2container , 
-	 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4, 
 	 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7, 
 	 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14, 
 	 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746, 
 	 nixpkgs-d919897915f0f91216d2501b617d670deee993a0, 
+	 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4, 
 	 }: let
 	  supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
 	  
@@ -40,11 +40,11 @@
 	  forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
 		inherit system;
 		 nix2containerPkgs = nix2container.packages.${system}; 
-		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs = import nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4 { inherit system; };
 		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs = import nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7 { inherit system; };
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs = import nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14 { inherit system; };
 		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs = import nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746 { inherit system; };
 		 nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs = import nixpkgs-d919897915f0f91216d2501b617d670deee993a0 { inherit system; };
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs = import nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4 { inherit system; };
 		
 		
 		pkgs = import nixpkgs { inherit system;  };
@@ -57,11 +57,11 @@
 		
 		
 		
-		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs, 
 		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs, 
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs, 
 		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs, 
 		 nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs, 
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs, 
 		 ... }: {
 		devShell = pkgs.mkShell {
 		  # The Nix packages provided in the environment
@@ -81,14 +81,11 @@
 		
 		
 		
-		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs,  ... }: {
+		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs,  nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  ... }: {
 		runtime = pkgs.buildEnv {
 		  name = "runtimeenv";
 		  paths = [ 
-			nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs.bash   
 			nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs.cacert   
-			nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs.coreutils-full   
-			nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs.file   
 			nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs.patchelfUnstable   
 			
 		   ];
@@ -99,7 +96,7 @@
 		
 		
 		
-	    nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs,  ... }: {
+	    nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs,  nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  ... }: {
 		development = pkgs.buildEnv {
 		  name = "devenv";
 		  paths = [ 
