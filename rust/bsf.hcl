04980ef44bb4ff11a72c2054455f015777e0167e
House <house@buildsafe.dev>
2024-07-22 00:52:51 +0530

    add patchelf, pin digests
    Signed-off-by: House <house@buildsafe.dev>

--- rust/bsf.hcl
+++ rust/bsf.hcl
@@ -1,29 +1,11 @@
 
 packages {
   development = ["coreutils-full@9.5", "bash@5.2.15", "cargo@1.75.0", "rustc@~1.73.0", "libiconv@~1.17", "clang-wrapper@~18.1.8"]
-  runtime     = ["cacert@3.95"]
-}
-
-rustapp {
-  workspaceSrc                  = "./."
-  projectName                   = "rust-todo"
-  release                       = true
-  rustVersion                   = "1.75.0"
-  rustToolchain                 = ""
-  rustChannel                   = ""
-  rustProfile                   = ""
-  rootFeatures                  = null
-  fetchCrateAlternativeRegistry = ""
-  hostPlatformCPU               = ""
-  hostPlatformFeatures          = null
-  extraRustComponents           = null
-  cargoUnstableFlags            = null
-  rustcLinkFlags                = null
-  rustcBuildFlags               = null
+  runtime     = ["cacert@3.95", "bash@5.2.15", "coreutils-full@9.5", "file@~5.45",  "patchelf@~0.18.0-unstable-2024-06-15"]
 }
 
 oci "pkgs" {
-  name          = "ttl.sh/rust-base:dev-7h"
+  name          = "ttl.sh/rust-base"
   cmd           = []
   entrypoint    = []
   envVars       = []
--- rust/bsf/flake.nix
+++ rust/bsf/flake.nix
@@ -3,11 +3,11 @@
 	description = "";
 	
 	inputs = {
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4.url = "github:nixos/nixpkgs/7445ccd775d8b892fc56448d17345443a05f7fb4";
 		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7.url = "github:nixos/nixpkgs/1ebb7d7bba2953a4223956cfb5f068b0095f84a7";
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14.url = "github:nixos/nixpkgs/ac5c1886fd9fe49748d7ab80accc4c847481df14";
 		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746.url = "github:nixos/nixpkgs/a731d0cb71c58f56895f71a5b02eda2962a46746";
 		 nixpkgs-d919897915f0f91216d2501b617d670deee993a0.url = "github:nixos/nixpkgs/d919897915f0f91216d2501b617d670deee993a0";
-		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4.url = "github:nixos/nixpkgs/7445ccd775d8b892fc56448d17345443a05f7fb4";
 			
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 		
@@ -15,8 +15,6 @@
 		
 		
 		
-		 cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-    	 nixpkgs.follows = "cargo2nix/nixpkgs";
 		
 		
 
@@ -27,82 +25,43 @@
 	outputs = inputs@{ self, nixpkgs, 
 	
 	
-	 cargo2nix, 
+	
 	
 	 nix2container , 
+	 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4, 
 	 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7, 
 	 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14, 
 	 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746, 
 	 nixpkgs-d919897915f0f91216d2501b617d670deee993a0, 
-	 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4, 
 	 }: let
 	  supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
 	  
-	  rustPkgs = pkgs: pkgs.rustBuilder.makePackageSet {
-		packageFun = import ./Cargo.nix;
-		workspaceSrc = ../.;
-		
-		rustVersion = "1.75.0"; 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	  }; 
 	  
 	  forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
 		inherit system;
 		 nix2containerPkgs = nix2container.packages.${system}; 
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs = import nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4 { inherit system; };
 		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs = import nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7 { inherit system; };
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs = import nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14 { inherit system; };
 		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs = import nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746 { inherit system; };
 		 nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs = import nixpkgs-d919897915f0f91216d2501b617d670deee993a0 { inherit system; };
-		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs = import nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4 { inherit system; };
 		
 		
-		pkgs = import nixpkgs { inherit system;  overlays = [cargo2nix.overlays.default];  };
+		pkgs = import nixpkgs { inherit system;  };
 		
 		
 	  });
 	in {
 	
-	packages = forEachSupportedSystem ({ pkgs,
-		
-		
-		
-		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs, 
-		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs, 
-		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs, 
-		 nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs, 
-		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs, 
-		 ... }: {
-		default = pkgs.callPackage ./default.nix {
-			
-			
-			
-			 inherit pkgs;
-             inherit rustPkgs;
-			
-			
-		};
-	  });
-	
 	  devShells = forEachSupportedSystem ({ pkgs, 
 		
 		
 		
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs, 
 		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs, 
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs, 
 		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs, 
 		 nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs, 
-		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs, 
 		 ... }: {
 		devShell = pkgs.mkShell {
 		  # The Nix packages provided in the environment
@@ -122,11 +81,15 @@
 		
 		
 		
-		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs,  nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  ... }: {
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs,  ... }: {
 		runtime = pkgs.buildEnv {
 		  name = "runtimeenv";
 		  paths = [ 
+			nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs.bash   
 			nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs.cacert   
+			nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs.coreutils-full   
+			nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs.file   
+			nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs.patchelfUnstable   
 			
 		   ];
 		};
@@ -136,7 +99,7 @@
 		
 		
 		
-	    nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs,  nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  ... }: {
+	    nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-d919897915f0f91216d2501b617d670deee993a0-pkgs,  ... }: {
 		development = pkgs.buildEnv {
 		  name = "devenv";
 		  paths = [ 
@@ -160,7 +123,7 @@
 
 		
 		ociImage_pkgs_runtime = nix2containerPkgs.nix2container.buildImage {
-			name = "ttl.sh/rust-base:dev-7h";
+			name = "ttl.sh/rust-base";
 			config = {
 				cmd = [  ];
 
@@ -184,7 +147,7 @@
 		};
 
 		ociImage_pkgs_dev = nix2containerPkgs.nix2container.buildImage {
-			name = "ttl.sh/rust-base:dev-7h";
+			name = "ttl.sh/rust-base";
 			config = {
 				cmd = [  ];
 
