26b79a5fe58491b8b69015c3b05fdb1631734734
hanshal101 <122217807+hanshal101@users.noreply.github.com>
2024-10-11 12:24:33 +0530

    feat: add ruby server example

---
+++ ruby-server-example/bsf/flake.nix
@@ -0,0 +1,163 @@
+
+{
+	description = "";
+	
+	inputs = {
+		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7.url = "github:nixos/nixpkgs/1ebb7d7bba2953a4223956cfb5f068b0095f84a7";
+		 nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191.url = "github:nixos/nixpkgs/2af19cfb6aa40768c4bbefd801a136270e099191";
+		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14.url = "github:nixos/nixpkgs/ac5c1886fd9fe49748d7ab80accc4c847481df14";
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4.url = "github:nixos/nixpkgs/7445ccd775d8b892fc56448d17345443a05f7fb4";
+			
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		
+		
+		
+		
+		
+		
+		
+
+		 
+		 nix2container.url = "github:nlewo/nix2container";
+	};
+	
+	outputs = inputs@{ self, nixpkgs, 
+	
+	
+	
+	
+	 nix2container , 
+	 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7, 
+	 nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191, 
+	 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14, 
+	 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4, 
+	 }: let
+	  supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+	  
+	  
+	  forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+		inherit system;
+		 nix2containerPkgs = nix2container.packages.${system}; 
+		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs = import nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7 { inherit system; };
+		 nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs = import nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191 { inherit system; };
+		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs = import nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14 { inherit system; };
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs = import nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4 { inherit system; };
+		
+		
+		pkgs = import nixpkgs { inherit system;  };
+		
+		
+	  });
+	in {
+	
+	  devShells = forEachSupportedSystem ({ pkgs, 
+		
+		
+		
+		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs, 
+		 nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs, 
+		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs, 
+		 nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs, 
+		 ... }: {
+		devShell = pkgs.mkShell {
+		  # The Nix packages provided in the environment
+		  packages =  [
+			nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs.bash  
+			nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs.bundler  
+			nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs.coreutils-full  
+			nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs.ruby  
+			
+		  ];
+		};
+	  });
+	
+	  runtimeEnvs = forEachSupportedSystem ({ pkgs,
+		
+		
+		
+		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  ... }: {
+		runtime = pkgs.buildEnv {
+		  name = "runtimeenv";
+		  paths = [ 
+			nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs.cacert   
+			
+		   ];
+		};
+	   });
+
+	   devEnvs = forEachSupportedSystem ({ pkgs,
+		
+		
+		
+	    nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  ... }: {
+		development = pkgs.buildEnv {
+		  name = "devenv";
+		  paths = [ 
+			nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs.bash  
+			nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs.bundler  
+			nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs.coreutils-full  
+			nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs.ruby  
+			
+		   ];
+		};
+	   });
+       
+	   
+	   
+	   
+
+ociImage_pkgs = forEachSupportedSystem ({ pkgs, nix2containerPkgs, system ,  nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs,  ... }: {
+  
+
+  
+  ociImage_pkgs_base = nix2containerPkgs.nix2container.buildImage {
+    name = "ttl.sh/ruby-server";
+    config = {
+      cmd = [  ];
+      entrypoint = [  ];
+      env = [
+        
+      ];
+      ExposedPorts = {
+        
+      };
+    };
+    maxLayers = 100;
+    layers = [
+       (nix2containerPkgs.nix2container.buildLayer { 
+			copyToRoot = [
+				nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs.cacert
+			];
+		})  (nix2containerPkgs.nix2container.buildLayer { 
+			copyToRoot = [
+				nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs.bash
+			];
+		})  (nix2containerPkgs.nix2container.buildLayer { 
+			copyToRoot = [
+				nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs.bundler
+			];
+		})  (nix2containerPkgs.nix2container.buildLayer { 
+			copyToRoot = [
+				nixpkgs-7445ccd775d8b892fc56448d17345443a05f7fb4-pkgs.coreutils-full
+			];
+		})  (nix2containerPkgs.nix2container.buildLayer { 
+			copyToRoot = [
+				nixpkgs-2af19cfb6aa40768c4bbefd801a136270e099191-pkgs.ruby
+			];
+		}) 
+      
+    ];
+  };
+  
+
+  
+  
+  ociImage_pkgs_base-as-dir = pkgs.runCommand "image-as-dir" { } "${inputs.self.ociImage_pkgs.${system}.ociImage_pkgs_base.copyTo}/bin/copy-to dir:$out";
+  
+  });
+
+
+
+	   
+	};
+}
